aaa713bc8d596112591049db0d7cf066fdfe0a9e
bmulmi <bmulmi@ramapo.edu>
2019-02-16 16:58:09 -0500

    second

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+var project = [{
+    title: "title",
+    description: "description",
+    languages: "C++"  
+  },
+  {
+    title: "title",
+    description: "description",
+    languages: "C++" 
+  }, 
+{
+  title: "title",
+  description: "description",
+  languages: "C++" 
+},
+{  title: "title",
+description: "description",
+languages: "C++" }];
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+    title: 'CodeBuddy', 
+    project: project
+  });
 });
 
 module.exports = router;
